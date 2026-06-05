c1e2fed46de748edc858e9411bf45ff03aabb348
anikaparekh2011 <anikaparekh2011@gmail.com>
2026-06-05 20:35:44 +0100

    Add files via upload

---
+++ summertimes.py
@@ -0,0 +1,16 @@
+print("Please choose one of the following that correspond with your weather at the moment")
+print("hot, warm, cold, raining, snowing.")
+weather = str(input("enter here: "))
+
+if (weather == "hot"):
+    print("you should wear shorts") 
+elif (weather == "warm"):
+    print("you should wear jeans")
+elif (weather == "cold"):
+    print("you should wear full-sleeves shirt")
+elif (weather == "raining"):
+    print("wear a jacket and take an umbrella ")
+elif (weather == "snowing"):
+    print("wear your pyjamas, you are staying inside ;) ")
+
+
